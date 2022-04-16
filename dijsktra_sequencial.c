eb9c932f4f36f0c1240bb309e05659be7ff2d6e7
Lucas Felipe <lcky123hop@gmail.com>
2022-04-16 17:12:46 -0300

    master

--- dijsktra_sequencial.c
+++ dijsktra_sequencial.c
@@ -9,35 +9,19 @@
 int distancias[TOTALCIDADES * TOTALCIDADES];
 double custos[TOTALCIDADES];
 
-/*
-  Funcao criaGrafo
-  - Rand para o total de ligacoes que o grafo irá ter
-  - Rand para qual cidade origem e destino
-  - Verifica se nao eh a mesma cidade e se ja nao possui ligacao entre as mesmas
-  - Rand para a distância
-  - Vetor distancias:
-      - Pos 0 ate (TOTALCIDADES - 1) = cidade 1
-      - Pos (TOTALCIDADES) ate ((2*TOTALCIDADES) -1) = cidade 2
-*/
-
-void criaGrafo()
-{
+void criaGrafo(){
     int origem, destino, totalLigacoes, i, ok, distancia;
 
     totalLigacoes = rand() % (TOTALCIDADES * 4);
     printf("TOTAL LIGACOES: %i\n", totalLigacoes);
 
-    for (i = 0; i < totalLigacoes; i++)
-    {
+    for (i = 0; i < totalLigacoes; i++){
         ok = 0;
-        while (ok == 0)
-        {
+        while (ok == 0){
             origem = rand() % TOTALCIDADES;
             destino = rand() % TOTALCIDADES;
-            if (origem != destino)
-            {
-                if (distancias[(origem)*TOTALCIDADES + destino] == -1)
-                {
+            if (origem != destino){
+                if (distancias[(origem)*TOTALCIDADES + destino] == -1){
                     distancia = (rand() % 20) + 1;
                     distancias[(origem)*TOTALCIDADES + destino] = distancia;
                     ok = 1;
@@ -48,70 +32,44 @@ void criaGrafo()
     }
 }
 
-/*
-  Funcao menorCaminho
-  - Recebe a origem e destino para calculo
-  - Aloca vetor necessário
-  - Verifica as ligacoes que direta que a "cidade" possui
-  - Por fim, é feito o calculo do menor caminho
-  - Impresso o resultado
-*/
-
-void dijkstra(int origem, int destino)
-{
+void dijkstra(int origem, int destino){
     int *anterior, i, aux;
     int *verticesNoCaminho, calculo;
     double distMinima, auxDist;
 
     verticesNoCaminho = calloc(TOTALCIDADES, sizeof(int *));
-    if (verticesNoCaminho == NULL)
-    {
+    if (verticesNoCaminho == NULL){
         printf("ERROR: Erro na alocacao \n");
         printf("ERROR: Erro na alocacao \n");
         exit(-1);
     }
 
-    for (i = 0; i < TOTALCIDADES; i++)
-    {
+    for (i = 0; i < TOTALCIDADES; i++){
         verticesNoCaminho[i] = 0;
         if (distancias[(origem)*TOTALCIDADES + i] != -1)
-        {
             custos[i] = distancias[(origem)*TOTALCIDADES + i];
-        }
         else
-        {
             custos[i] = HUGE_VAL;
-        }
     }
 
     verticesNoCaminho[origem] = 1;
     custos[origem] = 0;
 
-    do
-    {
+    do{
         distMinima = HUGE_VAL;
-
-        for (i = 0; i < TOTALCIDADES; i++)
-        {
-            if (!verticesNoCaminho[i])
-            {
-                if (custos[i] >= 0 && custos[i] < distMinima)
-                {
+        for (i = 0; i < TOTALCIDADES; i++){
+            if (!verticesNoCaminho[i]){
+                if (custos[i] >= 0 && custos[i] < distMinima){
                     distMinima = custos[i];
                     aux = i;
                 }
             }
         }
-
-        if (distMinima != HUGE_VAL && aux != destino)
-        {
+        if (distMinima != HUGE_VAL && aux != destino){
             verticesNoCaminho[aux] = 1;
-            for (i = 0; i < TOTALCIDADES; i++)
-            {
-                if (!verticesNoCaminho[i])
-                {
-                    if (distancias[aux * TOTALCIDADES + i] != -1 && custos[aux] + distancias[aux * TOTALCIDADES + i] < custos[i])
-                    {
+            for (i = 0; i < TOTALCIDADES; i++){
+                if (!verticesNoCaminho[i]){
+                    if (distancias[aux * TOTALCIDADES + i] != -1 && custos[aux] + distancias[aux * TOTALCIDADES + i] < custos[i]){
                         custos[i] = custos[aux] + distancias[aux * TOTALCIDADES + i];
                     }
                 }
@@ -123,45 +81,25 @@ void dijkstra(int origem, int destino)
     printf("Custo: %f\n", custos[destino]);
 }
 
-/*
-  Funcao calculoDistancia
-  - Dois for's que chamam a funcao para calculo do menor caminho
-*/
-
-void calculoDistancia()
-{
+void calculoDistancia(){
     int i, j;
-
-    for (i = 0; i < TOTALCIDADES; i++)
-    {
+    for (i = 0; i < TOTALCIDADES; i++){
         for (j = 0; j < TOTALCIDADES; j++)
-        {
             dijkstra(i, j);
-        }
     }
 }
 
-/*
-  Todas as distancias e custos sao zeradas, pois na hora do algoritmo eh verificado
-  as cidades que tem ligacoes.
-*/
-
-void zeraDistancia()
-{
+void zeraDistancia(){
     int i;
     for (i = 0; i < TOTALCIDADES * TOTALCIDADES; i++)
-    {
         distancias[i] = -1;
-    }
 
     for (i = 0; i < TOTALCIDADES; i++)
-    {
         custos[i] = 0;
-    }
+
 }
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]){
     setlocale(LC_ALL, "Portuguese");
 
     float tempo;
