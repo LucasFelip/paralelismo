2de9c91188ae31ffef80847fe9760757fa64b60c
Lucas Felipe <lcky123hop@gmail.com>
2022-04-25 20:01:38 -0300

    master

--- mapas/criar_grafo.c
+++ mapas/criar_grafo.c
@@ -6,9 +6,11 @@
 
 #define TOTALCIDADES 5
 
+FILE *file;
 int distancias[TOTALCIDADES * TOTALCIDADES];
 
 void criaGrafo(){
+    file = fopen("mapa_de_5_elem.txt", "w");
     int origem, destino, totalLigacoes, i, ok, distancia;
     for (i = 0; i < totalLigacoes; i++){
         ok = 0;
@@ -20,14 +22,24 @@ void criaGrafo(){
                     distancia = (rand() % 20) + 1;
                     distancias[(origem)*TOTALCIDADES + destino] = distancia;
                     ok = 1;
-                    printf("- %i - %i - %i - \n", origem, destino, distancia);
+                    fprintf(file,"%i - %i - %i\\n", origem, destino, distancia);
                 }
             }
         }
     }
+    fclose(file);
 }
 
 void zeraDistancia(){
     for (int i = 0; i < TOTALCIDADES * TOTALCIDADES; i++)
         distancias[i] = -1;
+}
+
+int main(void){
+    srand((unsigned)TOTALCIDADES);
+    zeraDistancia();
+    criaGrafo();
+
+    printf("Mapa gerado");
+    return 0;
 }
